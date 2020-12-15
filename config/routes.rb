66476b53486c9b9c5ea9a0b762ecb0e42b3e75de
Felix de Perthuis <fdeperthuis@sparkling-partners.com>
2020-12-15 20:56:48 +0100

    hetoku push

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root: to 'cocktails#index'
-  resources :cocktails do
+  root to: 'cocktails#index'
+  resources :cocktails, except: :index do
   resources :ingredients, only: [ :new, :create ]
   end 
 end
